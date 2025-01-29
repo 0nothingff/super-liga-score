86bf53ed9e63eaa4606cee5c6a98cb64223824db
nothingff <170884425+0nothingff@users.noreply.github.com>
2025-01-29 22:41:43 +0100

    Update nothingff.lua

--- nothingff.lua
+++ nothingff.lua
@@ -85,7 +85,7 @@ monitorGUI()
 
 
 
-
+wait ("5")
 --gui
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
